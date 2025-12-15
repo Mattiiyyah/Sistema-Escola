2b2aeeb25e7568227cfd4acc96b69ca4f9966e7b
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-12-15 10:36:29 -0300

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import Login from './modules/Login.js';
+import Aluno from './modules/Aluno.js';
 
 const login = new Login('.form-login');
 login.init();
@@ -9,3 +10,8 @@ login.init();
 const cadastro = new Login('.form-cadastro');
 cadastro.init();
 
+const redefinir = new Login('.form-redefinir-senha');
+redefinir.init();
+
+const aluno = new Aluno('.form-aluno');
+aluno.init();
