5c6f599f364590c37a6eff97a1fab4424af88bf1
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-12-15 10:35:33 -0300

    correção para validação de status de pagamento

--- src/models/AlunoModel.js
+++ src/models/AlunoModel.js
@@ -4,9 +4,9 @@ const validator = require('validator');
 const AlunoSchema = new moongose.Schema({
     nome: { type: String, required: true },
     turma: { type: String, required: true },
-    nomeResponsavel: { type: String, required: true }, 
-    emailResponsavel: { type: String, required: true }, 
-    telefoneResponsavel: { type: String, required: true }, 
+    nomeResponsavel: { type: String, required: true },
+    emailResponsavel: { type: String, required: true },
+    telefoneResponsavel: { type: String, required: true },
     valor: { type: Number, required: true },
     pago: { type: Boolean, required: true },
     criadoEm: { type: Date, default: Date.now }
@@ -23,41 +23,44 @@ class Aluno {
 
     async register() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
         this.aluno = await AlunoModel.create(this.body);
     };
 
     valida() {
-        
+
         this.cleanUp();
 
         //validação para o nome do aluno
-        if(!this.body.nome || this.body.nome.trim().length === 0) this.errors.push('Adicione o nome do aluno');
+        if (!this.body.nome || this.body.nome.trim().length === 0) this.errors.push('Adicione o nome do aluno');
 
-        if(!/^[A-Za-zÀ-ÿ\s]+$/.test(this.body.nome)) this.errors.push('Nome do aluno só pode conter letras');  
+        if (!/^[A-Za-zÀ-ÿ\s]+$/.test(this.body.nome)) this.errors.push('Nome do aluno só pode conter letras');
 
         //validação turma
-        if(!this.body.turma) this.errors.push('Adicione o aluno a uma turma!');
+        if (!this.body.turma) this.errors.push('Adicione o aluno a uma turma!');
 
         //validação para o nome do responsável
-        if(!this.body.nome || this.body.nome.trim().length === 0) this.errors.push('Adicione o nome do responsável');
+        if (!this.body.nomeResponsavel || this.body.nomeResponsavel.trim().length === 0) this.errors.push('Adicione o nome do responsável');
 
-        if(!/^[A-Za-zÀ-ÿ\s]+$/.test(this.body.nome)) this.errors.push('Nome do responsável só pode conter letras');  
-        
+        if (!/^[A-Za-zÀ-ÿ\s]+$/.test(this.body.nomeResponsavel)) this.errors.push('Nome do responsável só pode conter letras');
+
         //email do responsável precisa ser valido
-        if(this.body.emailResponsavel && !validator.isEmail(this.body.emailResponsavel)) this.errors.push('E-mail inválido');
+        if (this.body.emailResponsavel && !validator.isEmail(this.body.emailResponsavel)) this.errors.push('E-mail inválido');
 
         //validação para o telefone
-        if(!this.body.telefoneResponsavel || this.body.telefoneResponsavel.length < 5) this.errors.push('Telefone inválido');
-    
+        if (!this.body.telefoneResponsavel || this.body.telefoneResponsavel.length < 5) this.errors.push('Telefone inválido');
+
 
         //validação para o valor
-        if(isNaN(this.body.valor)) this.errors.push('Digite o valor corretamente');
+        if (isNaN(this.body.valor)) this.errors.push('Digite o valor corretamente');
+
+        //validação para o pagamento
+        if (this.body.pago !== 'true' && this.body.pago !== 'false') this.errors.push('Selecione o status de pagamento');
     }
 
     cleanUp() {
-        for(const key in this.body) {
-            if(typeof this.body[key] !== 'string' && typeof this.body[key] !== 'number' && typeof this.body[key] !== 'boolean') {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string' && typeof this.body[key] !== 'number' && typeof this.body[key] !== 'boolean') {
                 this.body[key] = '';
             }
         }
@@ -65,8 +68,8 @@ class Aluno {
         this.body = {
             nome: this.body.nome,
             turma: this.body.turma,
-            nomeResponsavel: this.body.nomeResponsavel, 
-            emailResponsavel: this.body.emailResponsavel, 
+            nomeResponsavel: this.body.nomeResponsavel,
+            emailResponsavel: this.body.emailResponsavel,
             telefoneResponsavel: this.body.telefoneResponsavel,
             valor: this.body.valor,
             pago: this.body.pago
@@ -74,36 +77,36 @@ class Aluno {
     }
 
     async edit(id) {
-        if(typeof id !== 'string') return;
+        if (typeof id !== 'string') return;
         this.valida();
-        if(this.errors.length > 0) return;
-        this.aluno = await AlunoModel.findByIdAndUpdate(id, this.body, { new: true});
+        if (this.errors.length > 0) return;
+        this.aluno = await AlunoModel.findByIdAndUpdate(id, this.body, { new: true });
     }
 
     //métodos estáticos
     static async buscaporId(id) {
-        if(typeof id !== 'string') return;
+        if (typeof id !== 'string') return;
         const aluno = await AlunoModel.findById(id);
         return aluno;
     }
 
     static async buscaAlunos() {
         const alunos = await AlunoModel.find()
-            .sort({criadoEm: -1});
-        return alunos; 
+            .sort({ criadoEm: -1 });
+        return alunos;
     }
 
     static async buscaporStatus(status) {
-        const alunos = await AlunoModel.find({pago: status})
-            .sort({criadoEm: -1})
+        const alunos = await AlunoModel.find({ pago: status })
+            .sort({ criadoEm: -1 })
         return alunos;
     }
 
     static async delete(id) {
-        if(typeof id !== 'string') return;
-        const aluno = await AlunoModel.findOneAndDelete({_id: id});
+        if (typeof id !== 'string') return;
+        const aluno = await AlunoModel.findOneAndDelete({ _id: id });
         return aluno;
     }
 
-} 
+}
 module.exports = Aluno;
